919b4973431c0497bd6b2eb716a80c8fb27c96e9
Kiryu <13matheusilva@gmail.com>
2024-03-26 16:31:32 -0300

    card front style add

--- src/components/cards/cardFront/cardFront.js
+++ src/components/cards/cardFront/cardFront.js
@@ -1,10 +1,12 @@
 import React from "react";
 import cardLogo from '../../../images/card-logo.svg'
 
+import './cardFront.css'
+
 const CardFront = () => {
     return (
         <div className="card front">
-            <img src={cardLogo} alt="logo"></img>
+            <img src={cardLogo} alt="logo" className="logo"></img>
             <p className="cardNumber">0000 0000 0000 0000</p>
             <div className="cardInfo">
                 <p className="name">Jane Appleseed</p>
